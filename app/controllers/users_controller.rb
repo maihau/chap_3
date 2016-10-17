f3846d217b9d6e31f6a2c00549b15400c7b340de
maihau22 <maihau22071995@gmail.com>
2016-10-17 14:26:29 +0700

    het chuong 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,36 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
+  
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-
   
-  def index
-    @users = User.paginate(page: params[:page])
+
+   def index
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    if @user.blank?
-      flash[:danger]="not sign up"
-      redirect_to signup_path
-    end
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless true
   end
 
   def new
-   
-  	@user = User.new
+   @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-       log_in @user
-    	 flash[:success] = "Welcome to the Sample App!"
-       redirect_to @user
+      @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+
     else
       render 'new'
     end
   end
-
+  
   def edit
     #@user = User.find_by(params[:id])
   end
@@ -51,6 +50,8 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+
+  
 
   
   private
@@ -73,9 +74,8 @@ class UsersController < ApplicationController
       redirect_to root_url unless current_user?(@user)
     end
 
-  
-  def admin_user
+    def admin_user
       redirect_to(root_url) unless current_user.admin?
-  end
-end
+    end
+end  
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,7 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+  
   def current_user?(user)
     user == current_user
   end
@@ -17,7 +18,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -45,7 +46,6 @@ module SessionsHelper
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
   end
-
 
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
